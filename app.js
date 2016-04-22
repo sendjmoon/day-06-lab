fdd863535770a722796fb15278b8b7be8ca00a5b
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-22 00:01:40 -0700

    changed app.js to include table elements only once. also added css styling to all pages

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'];
+var stores = [];
+
 function calcCustPerHour(minHrlyCust, maxHrlyCust) {
   var rng = Math.floor(Math.random() * (maxHrlyCust - minHrlyCust) + minHrlyCust);
   return rng;
 }
-var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'];
-var stores = [];
 
 function Store(storeName, minHrlyCust, maxHrlyCust, avgCookiesPerCust) {
   this.storeName = storeName;
@@ -25,32 +26,45 @@ function Store(storeName, minHrlyCust, maxHrlyCust, avgCookiesPerCust) {
   this.dailyTotals();
 }
 
-function makeStoreData(store) {
+function createTable() {
   var appendStores = document.getElementById('append-stores');
-  var th = document.createElement('th');
-  th.textContent = ' ';
-  th.textContent = store.storeName;
-  appendStores.appendChild(th);
+  var thName = document.createElement('th');
+  thName.id = 'name-title';
+  thName.textContent = 'Store Name';
+  appendStores.appendChild(thName);
+
   for (var i = 0; i < hours.length; i++) {
-    var tr = document.createElement('tr');
-    tr.textContent = ' ';
-    tr.textContent = hours[i] + ': ' + store.salesPerHour[i] + ' cookies';
-    th.appendChild(tr);
+    var thHours = document.createElement('th');
+    thHours.id = 'hours-header';
+    thHours.textContent = hours[i];
+    appendStores.appendChild(thHours);
   }
-  var total = document.createElement('tr');
-  total.textContent = ' ';
-  total.textContent = 'Total: ' + store.total + ' cookies';
-  th.appendChild(total);
+
+  var thTotal = document.createElement('th');
+  thTotal.id = 'total-header';
+  thTotal.textContent = 'Total';
+  appendStores.appendChild(thTotal);
 }
 
-stores.push(new Store('Pike Place',17,88,5.2));
-stores.push(new Store('SeaTac Airport',6,24,1.2));
-stores.push(new Store('Southcenter',11,38,1.9));
-stores.push(new Store('Bellevue Square',20,48,3.3));
-stores.push(new Store('Alki',3,24,2.6));
+function makeStoreData(store) {
+  var appendStores = document.getElementById('append-stores');
 
-for (var i = 0; i < stores.length; i++) {
-  makeStoreData(stores[i]);
+  var trName = document.createElement('tr');
+  trName.id = 'store-name';
+  trName.textContent = store.storeName;
+  appendStores.appendChild(trName);
+
+  for (var i = 0; i < hours.length; i++) {
+    var tdCookies = document.createElement('td');
+    tdCookies.id = 'td-cookies';
+    tdCookies.textContent = store.salesPerHour[i];
+    trName.appendChild(tdCookies);
+  }
+
+  var tdTotal = document.createElement('td');
+  tdTotal.id = 'td-total';
+  tdTotal.textContent = store.total;
+  trName.appendChild(tdTotal);
 }
 
 //Forms & Events
@@ -72,10 +86,22 @@ var handleStoreSubmit = function(event){
   var newStore = new Store(storeName,minCust,maxCust,avgCookies);
   stores.push(newStore);
   makeStoreData(newStore);
-  // for(var i = 0; i < stores.length; i++) {
-  //   makeStoreData(stores[i]);
-  // }
   stores = [];
 };
 
+function currentStores() {
+  for (var i = 0; i < stores.length; i++) {
+    makeStoreData(stores[i]);
+  }
+}
+
 storeForm.addEventListener('submit', handleStoreSubmit);
+
+stores.push(new Store('Pike Place',17,88,5.2));
+stores.push(new Store('SeaTac Airport',6,24,1.2));
+stores.push(new Store('Southcenter',11,38,1.9));
+stores.push(new Store('Bellevue Square',20,48,3.3));
+stores.push(new Store('Alki',3,24,2.6));
+
+createTable();
+currentStores(stores);
